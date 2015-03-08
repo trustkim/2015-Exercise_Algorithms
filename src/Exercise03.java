17b3b003e2700fa4c461f6b69e1ac74129b898b5
trustkim <trustkim90@gmail.com>
2015-03-08 20:46:27 +0900

    연습문제 3번 추가

---
+++ src/Exercise03.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+public class Exercise03 {
+	public static double[][] combinations;
+	public static void main(String [] args){
+		long start = System.currentTimeMillis();
+		try {
+			Scanner kb = new Scanner(System.in);
+			int n=kb.nextInt(); combinations = new double[n+1][];
+			int k=kb.nextInt(); for(int i=0;i<=n;i++) combinations[i] = new double[k+1];
+			
+			System.out.println(memorize_combination(n,k));
+			
+			}catch(Exception e){e.printStackTrace();}
+		System.out.println("Elapsed: "+(((long)System.currentTimeMillis())-start)/1000.0);
+	}
+	public static double combination(int n, int k){
+		if(k==0) {return 1;}
+		else if(k==n) {return 1;}
+		else{
+			return combinations[n][k] = combination(n-1,k)+combination(n-1,k-1);
+		}
+	}
+	public static double memorize_combination(int n, int k){
+		if(k==0) {combinations[n][k]=1;return 1;}
+		else if(k==n) {combinations[n][k]=1;return 1;}
+		else{
+			combinations[n][k] = (combinations[n-1][k]==0?memorize_combination(n-1,k):combinations[n-1][k])+(combinations[n-1][k-1]==0?memorize_combination(n-1,k-1):combinations[n-1][k-1]);
+			return combinations[n][k];
+		}
+	}
+}
