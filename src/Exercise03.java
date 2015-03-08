990708ff99b5655db8fd6d5367fdb16a4984b994
trustkim <trustkim90@gmail.com>
2015-03-08 20:53:02 +0900

    시간 측정 코드 위치 수정

--- src/Exercise03.java
+++ src/Exercise03.java
@@ -4,16 +4,15 @@ import java.util.Scanner;
 public class Exercise03 {
 	public static double[][] combinations;
 	public static void main(String [] args){
-		long start = System.currentTimeMillis();
 		try {
 			Scanner kb = new Scanner(System.in);
 			int n=kb.nextInt(); combinations = new double[n+1][];
 			int k=kb.nextInt(); for(int i=0;i<=n;i++) combinations[i] = new double[k+1];
 			
+			long start = System.currentTimeMillis();
 			System.out.println(memorize_combination(n,k));
-			
+			System.out.println("Elapsed: "+(((long)System.currentTimeMillis())-start)/1000.0);
 			}catch(Exception e){e.printStackTrace();}
-		System.out.println("Elapsed: "+(((long)System.currentTimeMillis())-start)/1000.0);
 	}
 	public static double combination(int n, int k){
 		if(k==0) {return 1;}
