c0d221553274ff33fffb76b16ba2dc873d00bd80
mid-eum <mid-eum@TK-Pavilion>
2015-03-12 16:25:34 +0900

    3월 12일 수업 마치고 4번을 좀 더 간단하게 봐꿔 보고 5번을 완성함. 5번은.. 더 줄이고 싶은 마음이 사라진다.

--- src/Exercise02_4.java
+++ src/Exercise02_4.java
@@ -4,6 +4,7 @@ public class Exercise02_4 {
 		String str1 = "abc";
 		String str2 = "ab";
 		System.out.println(compare(str1, str2));
+		System.out.println(solution(str1,str2,0));
 	}
 	public static int compare(String str1, String str2){
 		int temp = str2.charAt(0)-str1.charAt(0);	
@@ -14,5 +15,11 @@ public class Exercise02_4 {
 				} else return str1.length()>str2.length()?1:-1;	// 더 긴쪽이 사전식 순서가 늦다
 			}return compare(str1.substring(1), str2.substring(1));	// 검사할 문자가 남은 경우
 		} else return temp>0?-1:1;	// 두 문자열의 첫번째 문자끼리 서로 비교하여 다르면 결과를 출력
+	}
+	
+	public static int solution(String str1, String str2, int begin) {
+		if(begin==Math.min(str1.length(), str2.length())){
+			return str1.length()>str2.length()?1:str1.length()<str2.length()?-1:0;
+		}else return str1.charAt(begin)>str2.charAt(begin)?1:str1.charAt(begin)<str2.charAt(begin)?-1:solution(str1,str2,begin+1);
 	}
 }
---
+++ src/Exercise02_5.java
@@ -0,0 +1,72 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Exercise02_5 {
+	private static int N;
+	private static int [][] maze;
+	
+	private static final int PATHWAY = 0;
+	private static final int WALL = 1;
+	private static final int BLOCKED = 2;
+	private static final int PATH = 3;
+	
+	public static void main(String [] args) {
+	
+		long start = System.currentTimeMillis();
+		try {
+			Scanner input = new Scanner(new File("input02_5.txt"));
+			
+			for(int T=input.nextInt();T>0;T--) {
+				N = input.nextInt();
+				int x0 = input.nextInt();
+				int y0 = input.nextInt();
+				int x1 = input.nextInt();
+				int y1 = input.nextInt();
+				maze = new int[N][N];
+				for(int i=0;i<N;i++)
+					for(int j=0;j<N;j++)
+						maze[i][j] = input.nextInt();
+				System.out.println(N+" "+x0+" "+y0+" "+x1+" "+y1);
+				Print(maze);
+				
+				System.out.println(findHorsePath(x0, y0, x1, y1));
+			}
+			
+			
+		} catch (FileNotFoundException e) {e.printStackTrace();}
+		System.out.println("Elapsed: "+(((long)System.currentTimeMillis())-start)/1000.0);
+	}
+	public static boolean findHorsePath(int x0, int y0, int x1, int y1) {
+		if(x0<0 || y0<0 || x0>N-1 || y0>N-1){
+			return false;	// 범위 넘어가면 안됨
+		}else if(maze[x0][y0] != PATHWAY)
+			return false;	// 갈수 없어도 안됨. 다른 말이 있거나 갈 수 없는 길로 판명난 길이거나
+		else if (x0==x1 && y0==y1) {
+			maze[x0][y0] = PATH;
+			return true;
+		}
+		else {
+			maze[x0][y0] = PATH;
+			if(x0-1>=0&&y0>=0&&x0-1<N&&y0<N&&maze[x0-1][y0]!=WALL)	// maze[x0-1][y0]==PATHWAY가 아닌 이유는? PATHWAY 말고도 BLOCKED나 PATH여도 상관 없다.
+				if(findHorsePath(x0-2,y0-1,x1,y1) || findHorsePath(x0-2, y0+1,x1,y1)) return true;
+			if (x0>=0&&y0+1>=0&&x0<N&&y0+1<N&&maze[x0][y0+1]!=WALL)
+				if(findHorsePath(x0-1,y0+2,x1,y1) || findHorsePath(x0+1,y0+2,x1,y1)) return true;
+			if(x0+1>=0&&y0>=0&&x0+1<N&&y0<N&&maze[x0+1][y0]!=WALL)
+				if(findHorsePath(x0+2,y0+1,x1,y1) || findHorsePath(x0+2,y0-1,x1,y1)) return true;
+			if(x0>=0&&y0-1>=0&&x0<N&&y0-1<N&&maze[x0][y0-1]!=WALL)
+				if(findHorsePath(x0+1,y0-2,x1,y1) || findHorsePath(x0-1,y0-2,x1,y1)) return true;
+
+			maze[x0][y0] = BLOCKED;
+			return false;
+		}
+	}
+	
+	public static void Print(int [][] grid) {
+		for(int i=0;i<grid.length;i++){
+			for(int j=0;j<grid[i].length;j++)
+				System.out.print(grid[i][j]+" ");
+			System.out.println();
+		}
+	}
+}
