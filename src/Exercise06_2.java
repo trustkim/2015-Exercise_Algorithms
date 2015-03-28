294df803ce39457d69737335cbabb68f3e9c70ef
trustkim <trustkim90@gmail.com>
2015-03-28 17:18:54 +0900

    실습6 2번 우선순위 큐를 위한 insert, extract 추가

--- src/Exercise06_2.java
+++ src/Exercise06_2.java
@@ -1,13 +1,53 @@
 
 public class Exercise06_2 {
+	private static int [] data = new int[100000];
+	private static int heapSize = 0;
 	public static void main(String[] args) {
-		int [] data = new int[100000];
+		int [] test = {20, 10, 15, 8, 7, 13, 14, 2, 5, 6};
+		for(int i=0;i<10;i++) {
+			data[i] = test[i];
+			heapSize++;
+		}	// 초기 큐를 세팅 해 둔다
+		buildMaxHeap(data, heapSize); // 초기 큐를 힙으로 만든다
+		for(int i=0;i<heapSize;i++)
+			System.out.print(data[i]+", ");	// 테스트 출력
+		System.out.println();
+		maxHeapInsert(data, 15);			// 15를 추가한다 (인큐)
+		for(int i=0;i<heapSize;i++)
+			System.out.print(data[i]+", ");
+		System.out.println();
+		
+		System.out.println(extractMax(data));	// 20을 디큐
+		for(int i=0;i<heapSize;i++)
+			System.out.print(data[i]+", ");		// 남은 큐 출력
+		System.out.println();
 	}
-	
-	private static void buildMaxHeap(int [] data) {
-		int heap_size = data.length;
-		for(int i=(heap_size-1)/2;i>=0;i--){
-			maxHeapify(data, i, heap_size);
+	private static void maxHeapInsert(int [] data, int key){
+		heapSize++;					// 힙 사이즈를 증가시킴
+		data[heapSize-1] = key;
+		int i = heapSize-1;
+		while(i>0 && data[(i-1)/2]<data[i]) {
+			int temp = data[i];
+			data[i] = data[(i-1)/2];
+			data[(i-1)/2] = temp;
+			i = (i-1)/2;
+		}
+	}
+	private static int extractMax(int[] data) {
+		if(heapSize<1) {
+			System.out.println("heap underflow");
+			return -1;
+		}
+		
+		int max = data[0];	// 루트 노드가 항상 max
+		data[0] = data[heapSize-1];	// 마지막 노드를 루트 노드에 덮어 씀
+		heapSize--;	// 힙 사이즈 1감소
+		maxHeapify(data, 0, heapSize);
+		return max;
+	}
+	private static void buildMaxHeap(int [] data, int size) {
+		for(int i=(size-1)/2;i>=0;i--){
+			maxHeapify(data, i, size);
 		}
 	}
 	private static void maxHeapify(int[] data, int i, int size){
