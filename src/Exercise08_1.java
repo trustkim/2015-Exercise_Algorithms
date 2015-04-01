701462d21e6ffacd0a0ef8a032e233881e700d33
mid-eum <mid-eum@TK-Pavilion>
2015-04-01 11:21:04 +0900

    실습8 문제1 추가. makeSample 함수는 스택으로 inorder binary tree traversal을 C++에서 구현할 때 썼던 append_branch 함수를 참고하였다.
    이는 Introduction to Algorithm을 참고했던 것.
    거의 까먹었지만 다시 코드를 보면 기억날 것 같다.
    리커전은 정말 간단하다...

---
+++ src/Exercise08_1.java
@@ -0,0 +1,107 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * 아래 그림과 같이 9개의 노드를 가진이진트리를만든 후 inorder, preorder, postorder, 그리고 level-order로
+ * 순회하면서 각 노드에 저장된 데이터를 출력하는 프로그램을 작성하라.
+ */
+public class Exercise08_1 {
+	public static void main(String[] args) {
+		BinaryTree binaryTree_test = new BinaryTree();
+		binaryTree_test.makeSampleTree();
+		
+		System.out.print("inorder traversal: ");
+		binaryTree_test.inorderTraversal(binaryTree_test.root);
+		
+		System.out.print("\npreorder traversal: ");
+		binaryTree_test.preorderTraversal(binaryTree_test.root);
+		
+		System.out.print("\npostorder traversal: ");
+		binaryTree_test.postorderTraversal(binaryTree_test.root);
+		
+		System.out.print("\nlevel-order traversal: ");
+		binaryTree_test.levelorderTraversal(binaryTree_test.root);
+	}
+}
+
+class Node {
+	String data;
+	Node left,right,parent;
+	Node() {
+		data = ""; left=null;right=null;parent=null;
+	}
+}
+
+class BinaryTree {
+	public Node root;
+	public BinaryTree() {
+		root = null;
+	}
+	public Node append_branch(Node root, String data) {
+		Node Cur = new Node();
+		Cur.data = String.copyValueOf(data.toCharArray());
+		//System.out.println(Cur.data);
+		
+		if(root==null) {
+			root = Cur;
+		}else {
+			Node P = root; int sw = 1;
+			while(sw==1) {
+				if(P.data.compareTo(Cur.data)>=0){
+					if(P.left==null) {
+						P.left = Cur; sw=0;
+					}else {
+						P = P.left;
+					}
+				}else {
+					if(P.right==null){
+						P.right = Cur; sw=0;
+					}else {
+						P = P.right;
+					}
+				}
+			}
+		}
+		return root;
+	}
+	public void makeSampleTree() {
+		String[] samples = {"fox", "bear", "goose", "ant", "dog", "hippo", "cat", "eagle", "iguana"};
+		System.out.print("Sample data: ");
+		for(int i=0;i<samples.length;i++) {
+			root = append_branch(root, samples[i]);
+			System.out.print(samples[i]+", ");
+		}
+		System.out.println();
+	}
+	public void inorderTraversal(Node x) {
+		if(x!=null){
+			inorderTraversal(x.left);
+			System.out.print(x.data+", ");
+			inorderTraversal(x.right);	
+		}
+	}
+	public void preorderTraversal(Node x) {
+		if(x!=null){
+			System.out.print(x.data+", ");
+			preorderTraversal(x.left);
+			preorderTraversal(x.right);	
+		}
+	}
+	public void postorderTraversal(Node x) {
+		if(x!=null){
+			postorderTraversal(x.left);
+			postorderTraversal(x.right);
+			System.out.print(x.data+", ");	
+		}
+	}
+	public void levelorderTraversal(Node root) {
+		Queue<Node> queue = new LinkedList<Node>();
+		queue.offer(root);
+		while(!queue.isEmpty()) {
+			Node node = queue.poll();
+			System.out.print(node.data+", ");
+			if(node.left!=null) queue.offer(node.left);
+			if(node.right!=null) queue.offer(node.right);
+		}
+	}
+}
