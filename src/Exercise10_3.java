5de1ec6182c9337acb6b7c1e33fde769ae736faf
mid-eum <mid-eum@TK-Pavilion>
2015-04-16 16:25:05 +0900

    실습10 문제3 문제만 추가해둠. 함수는 실제로 문제2에서 확인 가능

---
+++ src/Exercise10_3.java
@@ -0,0 +1,10 @@
+/*
+ * Java의 TreeMap 클래스는 매우 다양한 메서드들을 제공한다. 그 중 하나는 ceilingKey(K Key)와
+ * floorKey(K key) 메서드이다. ceilingKey 메서드는 주어진 키보다 크거나 같으면서 가장 작은 키값을
+ * 찾아서 반환한다. 그런 키가 없을 경우 null을 반환한다. floorKey 메서드는 반대로 주어진 키보다 작거나
+ * 같으면서 가장 큰 키를 반환한다. 물론 그런 키가 없으면 null을 반환한다. 2번에서 구현한 프로그램에 이 두
+ * 가지 메서드를 추가하라.
+ */
+public class Exercise10_3 {
+
+}
