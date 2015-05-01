f9a34a497201b8b9c299340d5eefbf52d568f6e6
mid-eum <mid-eum@TK-Pavilion>
2015-05-01 14:06:30 +0900

    일부 배열 인덱스 final 변수로 미리 선언해 둠.

--- src/Exercise12_2.java
+++ src/Exercise12_2.java
@@ -19,6 +19,11 @@ import java.util.Scanner;
  * 도시간의 거리를 계산할 때 이 함수를 이용하라. 이 함수가 반환하는 거리의 단위는 m이다.
  */
 public class Exercise12_2 {
+	private static final int MAXCITY = 200000;				// 최대로 읽을 수 있는 도시 수.
+	private static final int UNVISITED = -1;
+	private static final int NAME = 1;
+	private static final int LATITUDE = 3;					// 위도.
+	private static final int LONGITUDE = 2;					// 경도.
 	static class City {
 		int id;
 		String name;
@@ -32,7 +37,7 @@ public class Exercise12_2 {
 	}
 	private static MyRBTreeMap<String, Integer> rbm;		// <도시이름, 일련번호> 쌍 맵. 이걸 쓰면 인접리스트에서 도시이름을 키로 해당 도시의 일련번호를 더 빨리 찾을 수 있다.
 	private static LinkedList<City>[] adjList;				// 각 도시를 노드로하는 인접 리스트
-	private static City[] tableBuffer = new City[200000];
+	private static City[] tableBuffer = new City[MAXCITY];
 	@SuppressWarnings("unchecked")
 	public static void main(String [] args){	
 		rbm = new MyRBTreeMap<String, Integer>();
@@ -44,8 +49,8 @@ public class Exercise12_2 {
 			City city;		// 한 도시 객체 임시 변수.
 			while(sc.hasNextLine()){
 				temp = sc.nextLine().split("\\t");				// 파일에서 한 라인을 읽어 "\\t"으로 자름. 백슬래시가 두 개!
-				addCityName(temp[1],temp[1],cityID,0);			// 트리맵에 <도시이름, 일련번호> 쌍을 추가함.
-				city = new City(cityID, temp[1], Double.parseDouble(temp[3]), Double.parseDouble(temp[2]));
+				addCityName(temp[NAME],temp[NAME],cityID,0);			// 트리맵에 <도시이름, 일련번호> 쌍을 추가함.
+				city = new City(cityID, temp[NAME], Double.parseDouble(temp[LATITUDE]), Double.parseDouble(temp[LONGITUDE]));
 				tableBuffer[cityID] = city;						// 일단 인접리스트의 크기를 모르므로 한 파일을 다 읽을 동안 모든 도시 정보를 테이블에 저장해둠.
 				cityID++;
 			}	// file read complete
@@ -116,20 +121,20 @@ public class Exercise12_2 {
 	private static void PrintByHop(String start, final int k) {
 		System.out.println("PrintByHop from "+start+"...");
 		if(rbm.containsKey(start)) {  
-			int[] checkTable = new int[adjList.length];			// BFS에서 한 번 갔던 노드를 다시 방문하지 않게 하는 체크 테이블.
+			int[] checkTable = new int[adjList.length];			// BFS에서 한 번 갔던 노드를 다시 방문하지 않게 하는 체크 테이블. 동시에 hop을 카운트하는 배열이다.
 			for(int i=0;i<checkTable.length;i++) {
-				checkTable[i] = -1;								// -1로 초기화
+				checkTable[i] = UNVISITED;						// -1로 초기화
 			}
 			Queue<Integer> queue = new LinkedList<Integer>();	// BFS를 위한 큐. 큐에는 각 도시 일련번호를 저장하여 BFS시 접근할 도시의 인덱스를 반환하게 한다.
 			int startIndex = rbm.get(start);
 			queue.offer(startIndex);							// 시작 id의 도시에 대한 인접리스트를 인큐.
-			checkTable[startIndex] = 0;							// 시작 노드 체크.
+			checkTable[startIndex] = 0;							// 시작 노드 체크. 이런 건 명시적으로 언급해주는게 좋다고 하심.
 			int curIndex;										// BFS 한 스텝에 디큐받을 변수.
 			while(!queue.isEmpty()) {
 				curIndex = queue.poll();						// 이번에 방문한 id의 도시 인접리스트를 디큐.
 				for(City i:adjList[curIndex]) {					// 한 인접리스트 인덱스를 전부 순회함. 즉 현재 방문한 노드의 모든 방문하지 않았던 인접 노드를 방문.
 					int nextIndex = rbm.get(i.name);			// 이터래이터 i의 name으로 id를 찾음.
-					if(checkTable[nextIndex]<0 && checkTable[curIndex]<k) {
+					if(checkTable[nextIndex]==UNVISITED && checkTable[curIndex]<k) {
 						checkTable[nextIndex] = checkTable[curIndex]+1;		// 방문한 인덱스 체크
 						System.out.println(checkTable[nextIndex]+": "+i.name);
 						queue.offer(nextIndex);					// 인접한 모든 노드를 인큐.
