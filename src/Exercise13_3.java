e6f56d137994d36cb2cbff05b8daaad2fb958e55
mid-eum <mid-eum@TK-Pavilion>
2015-05-05 19:47:45 +0900

    위상 정렬 알고리즘2의 개선 사항 업데이트

--- src/Exercise13_3.java
+++ src/Exercise13_3.java
@@ -18,26 +18,28 @@ import java.util.Scanner;
  * 은 상수값이다.  
  */
 public class Exercise13_3 {
-	private static final int L_VALUE = 0;
-	private static HashMap<String, Integer> hsm;
-	private static Node[] g;
-	private static int[] visited;	// 위상정렬 알고리즘2를 위한 배열.
-	private static Node R;			// 위상정렬 알고리즘2를 위한 연결리스트.
-	private static class Node {
+	private final static int L_VALUE = 0;
+	private static HashMap<String, Integer> hsm;	// 항 수를 세기 위한 해쉬맵
+	private static Node[] adjList;
+	private int[] outdegree;
+	private int[] visited;							// 위상정렬 알고리즘2를 위한 배열.
+	private Node R;									// 위상정렬 알고리즘2를 위한 연결리스트.
+	private class Node {
 		String data;
-		int outdeg;
 		Node next;
 		Node(String x) {
 			data = String.copyValueOf(x.toCharArray());
-			outdeg=0;	next=null;
+			next=null;
 		}
 	}
-	public static void main(String[] args) {
+	
+	private static String[] fileRead(String filename) {
+		String[] notsBuffer=null;
 		try {
 			Scanner sc = new Scanner(new File("input13_3.txt"));
 			int N = sc.nextInt(); sc.nextLine();
 			hsm = new HashMap<String, Integer>();
-			String[] notsBuffer = new String[N];
+			notsBuffer = new String[N];
 			int i=0, id=1; String[] nots;
 			while(sc.hasNextLine()){
 				notsBuffer[i] = sc.nextLine();
@@ -51,26 +53,13 @@ public class Exercise13_3 {
 				}
 				i++;
 			} // file read complete
-			
-			// 인접리스트 만들기.
-			g = new Node[hsm.size()+1];	// [0]은 사용하지 않는다.
-			constructAdjList(notsBuffer,g);
-			testPrintAdj(g);
-			
-			// 위상 정렬 수행.
-			String[] calcOrder = new String[hsm.size()+1];
-			topologicalSort2(calcOrder);
-			PrintTopol(calcOrder);
-			
-			// 위상 정렬한 순서대로 연산 수행.
-			System.out.println("res = " + calcTopolOrder(calcOrder));
-			
 			sc.close();
 		}catch(FileNotFoundException e) {System.out.println("file not found...");}
+		return notsBuffer.clone();
 	}
 	
 	/* 인접리스트 만들기 */
-	private static void add(Node r, String var) {
+	private void add(Node r, String var) {
 		Node cur = new Node(var);		// 추가할 노드.
 		Node p = r;						// 추가할 위치.
 		while(p.next!=null) {
@@ -78,51 +67,58 @@ public class Exercise13_3 {
 		}
 		p.next = cur;
 		// 인접리스트의 헤드 노드들에 outdegree를 저장 해 둠. 나머지는 모두 0이다.
-		r.outdeg++;
+		outdegree[hsm.get(r.data)]++;
 	}
-	private static void constructAdjList(String[] buf, Node[] g) {
+	private void constructAdjList(String[] buf) {
+		adjList = new Node[hsm.size()+1];	// [0]은 사용하지 않는다.
+		outdegree = new int[hsm.size()+1];
 		String[] nots;
 		for(int i=0;i<buf.length;i++) {
 			nots = buf[i].split(" ");	// 어쩔 수 없이 두 번 토크나이징 함.
 			String leftValue = nots[L_VALUE];
 			int leftIndex = hsm.get(leftValue);
-			if(g[leftIndex]==null)
-				g[leftIndex] = new Node(leftValue);
+			if(adjList[leftIndex]==null)
+				adjList[leftIndex] = new Node(leftValue);
 			for(int j=2;j<nots.length;j+=2) {	// 수식의 오른쪽 부분을 읽어 처리.
 				int rightIndex = hsm.get(nots[j]);
-				if(g[rightIndex]==null)
-					g[rightIndex] = new Node(nots[j]);
-				add(g[rightIndex],leftValue);
+				if(adjList[rightIndex]==null)
+					adjList[rightIndex] = new Node(nots[j]);
+				add(adjList[rightIndex],leftValue);
 			}
 		}
 	}
 	
 	/* 위상정렬 알고리즘 2 */
-	private static boolean DFS_TS(String v) {
+	private boolean DFS_TS(Node[] adjList, String v) {
 		boolean isDAG = true;
-		visited[hsm.get(v)] = 1;
-		Node x = g[hsm.get(v)].next;
-		while(x!=null) {
+		visited[hsm.get(v)]++;
+		Node x = adjList[hsm.get(v)].next;
+		while(x!=null && isDAG) {
 			if(visited[hsm.get(x.data)]==0)
-				isDAG = DFS_TS(x.data);
-			else if(R==null && g[hsm.get(x.data)].outdeg>0) {
+				isDAG = DFS_TS(adjList, x.data);
+			else if(visited[hsm.get(x.data)]==1) {
 				return false;	// 위상 정렬시 마지막에 와야 하는 노드의 outdegree가 0이 아닌경우 DAG가 아님.
 			}
 			x = x.next;
 		}
-		x = new Node(v);
+		
+		// R의 앞에 추가.
+		x = new Node(adjList[hsm.get(v)].data);
 		x.next = R;
 		R = x;
+		visited[hsm.get(v)]++;	// DFS를 거슬러 올라가면서 두번째 체크.
 		return isDAG;
 	}
-	private static boolean topologicalSort2(String[] A) {
-		visited = new int[g.length];	// [0]은 사용하지 않음.
+	private boolean topologicalSort2(String[] A) {
+		visited = new int[adjList.length];	// [0]은 사용하지 않음. DFS 수행 시 최대 두 번까지 체크됨.
 		R = null;
-		for(int i=1;i<g.length;i++) {
+		for(int i=1;i<adjList.length;i++) {
 			if(visited[i]==0) {
-				if(!DFS_TS(g[i].data)) return false;
+				if(!DFS_TS(adjList, adjList[i].data)) return false;
 			}
 		}
+
+		// R을 A로 바꿔서 반환함. 굳이 안해줘도 상관 없다.
 		for(int i=1;i<A.length;i++) {
 			A[i] = R.data;
 			R = R.next;
@@ -131,13 +127,13 @@ public class Exercise13_3 {
 	}
 	
 	/* 위상 정렬된 배열을 이용하여 수식 계산 */
-	private static boolean isVar(String v) {	// 입력 스트링이 변수인지 상수인지 판단하는 함수.
+	private boolean isVar(String v) {	// 입력 스트링이 변수인지 상수인지 판단하는 함수.
 		return (v.compareTo("A")>=0&&v.compareTo("Z")<=0);
 	} // 변수이면 true, 상수이면 false를 반환.
-	private static int calcTopolOrder(String[] A) {
+	private int calcTopolOrder(String[] A) {
 		int[] varTable = new int[hsm.size()+1];	// 변수 테이블 생성.
 		for(int i=1;i<A.length;i++) {	// 위상 정렬된 배열에서 순서대로 데이터를 읽음.
-			Node ld = g[hsm.get(A[i])];	
+			Node ld = adjList[hsm.get(A[i])];	
 			Node st = ld.next;			// 더한 결과를 저장할 주소 노드.
 			while(st!=null) {	// ld의 인접한 모든 노드 st에 ld의 값을 더함.
 				if(isVar(A[i]))	// 변수이면 변수 테이블에서 값을 불러 옮.
@@ -151,11 +147,11 @@ public class Exercise13_3 {
 	}
 	
 	/* 테스트 및 출력을 위한 함수 */
-	private static void testPrintAdj(Node[] list) {
+	private void testPrintAdj(Node[] list) {
 		System.out.println("====== TEST PRINT : AdjList ======");
 		for(int i=1;i<list.length;i++) {
 			Node cur = list[i];
-			System.out.print(cur.data+"("+cur.outdeg+"): ");
+			System.out.print(cur.data+"("+outdegree[hsm.get(cur.data)]+"): ");
 			cur = cur.next;
 			while(cur!=null) {
 				System.out.print(cur.data+", ");
@@ -165,10 +161,26 @@ public class Exercise13_3 {
 		}
 		System.out.println("====== TEST PRINT : AdjList ======");
 	}
-	private static void PrintTopol(String[] A) {
+	private void PrintTopol(String[] A) {
 		System.out.print("topolOrder: ");
 		for(int i=1;i<A.length;i++)
 			System.out.print(A[i]+" ");
 		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		Exercise13_3 test = new Exercise13_3();
+
+		// 인접리스트 만들기.
+		test.constructAdjList(fileRead("input13_2.txt"));
+		test.testPrintAdj(adjList);
+
+		// 위상 정렬 수행.
+		String[] calcOrder = new String[hsm.size()+1];
+		test.topologicalSort2(calcOrder);
+		test.PrintTopol(calcOrder);
+
+		// 위상 정렬한 순서대로 연산 수행.
+		System.out.println("res = " + test.calcTopolOrder(calcOrder));
 	}
 }
